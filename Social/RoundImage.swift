4741df6bf38c9240866f36f5241f5801bac6b531
Karthik Varma <kvarma@adt.com>
2017-06-15 23:42:06 -0500

    UI Complete, without outlet connections

--- Social/RoundImage.swift
+++ Social/RoundImage.swift
@@ -1,22 +1,26 @@
 //
-//  CustomHeaderView.swift
+//  RoundImage.swift
 //  Social
 //
-//  Created by Karthik Varma on 6/11/17.
+//  Created by Karthik Varma on 6/15/17.
 //  Copyright © 2017 Karthik. All rights reserved.
 //
 
 import UIKit
 
-class CustomHeaderView: UIView {
-
+class RoundImage: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.shadowColor = UIColor(red: kShadowGray, green: kShadowGray, blue: kShadowGray, alpha: kShadowGray).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 8
         layer.shadowOffset = CGSize(width: 1, height: 1)
-        layer.cornerRadius = 2.0
-        
+        self.contentMode = .scaleAspectFit
     }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+    }
+
 }
